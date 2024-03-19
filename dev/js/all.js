78cfdb1878f66a77956495b5adf1919dc8eda06b
kateweb988 <katyavov2805@gmail.com>
2024-03-19 12:38:58 +0200

    last

---
+++ dev/js/all.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Scroll
+  $('.go_to').click(function () { // ловим клик по ссылке с классом go_to
+    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 50 }, 800); // анимируем скроолинг к элементу scroll_el
+    }
+    return false; // выключаем стандартное действие
+  });
+});
+// document.addEventListener("DOMContentLoaded", () => {
+//   let btn = document.querySelector('.phone')
+//   //
+//   function magic() {
+//     if (window.pageYOffset > 100) {
+//       btn.style.display = 'block'
+//     } else { btn.style.display = 'none' }
+//   }
+//   //
+//   btn.onclick = function () {
+//     window.scrollTo(0, 0)
+//   }
+
+//   // When scrolling, we run the function
+//   window.onscroll = magic
+// });
+document.addEventListener("DOMContentLoaded", () => {
+  var basicScrollTop = function () {
+    var btnTop = document.querySelector('.phone');
+    var btnReveal = function () {
+      if (window.scrollY >= 100) {
+        btnTop.classList.add('is-visible');
+      } else {
+        btnTop.classList.remove('is-visible');
+      }
+    };
+    window.addEventListener('scroll', btnReveal);
+  };
+  basicScrollTop();
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $('.menu li a').click(function (event) {
+    $('.menu-btn').toggleClass('active');
+    $('.menu').toggleClass('active');
+    return false;
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  let menuBtn = document.querySelector('.menu-btn');
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
+// svg
+$(function () {
+  jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, else we gonna set it if we can.
+      if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
+
+  });
+});
